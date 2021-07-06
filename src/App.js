20ea87e5919f56c23f253019f67ecdb0a1460506
Rodrigo Barreto <barretorodrigo@gmail.com>
2021-07-05 21:49:28 -0300

    Integração do layout com material, link nos cartões e filtragem

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Movies from './pages/Movies';
 import SingleMovie from './pages/SingleMovie';
+import PersistentDrawerLeft from './PersistentDrawerLeft';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <SingleMovie />
         </Route>
         <Route path="/">
-          <Movies />
+          <PersistentDrawerLeft />
         </Route>
       </Switch>
     </BrowserRouter>
